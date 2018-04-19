3ba8d5372f478ea5cb556bb503c7672fcd83fab6
Chris Conolly <chris.conolly@wonga.com>
2018-04-19 11:55:13 +0100

    background now moves if char at boundry

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { merge } from 'rxjs/observable/merge';
-import { LerpService } from '../../services/lerp/lerp.service';
 import { Observable, Scheduler } from 'rxjs/Rx';
+
+import { LerpService } from '../../services/lerp/lerp.service';
+import { CharPositionOnScreenService } from '../../services/char-position-on-screen/char-position-on-screen.service';
 
 @Component({
   selector: 'background',
@@ -15,20 +17,31 @@ export class BackgroundComponent implements OnInit {
   speed = 200;
   lerp;
   calculateNewPosition;
+  charScreenPositionX;
 
-  constructor(private lerpService: LerpService) {
+  constructor(private lerpService: LerpService, private charPositionOnService: CharPositionOnScreenService) {
     this.lerp = lerpService.lerp;
     this.calculateNewPosition = lerpService.calculateNewPosition;
+    charPositionOnService.charScreenPosition$.subscribe(
+      val => this.charScreenPositionX = val
+    );
+  }
+
+  moveBackgroundIfOutOfBounds = (position, speed, positionOnScreen, boundaryPercentage) => {
+    if ((speed.x > 0 && positionOnScreen > boundaryPercentage) || (speed.x < 0 && positionOnScreen < boundaryPercentage)) {
+      return {x: position.x, y: position.y};
+    }
+    return this.calculateNewPosition(this.position, speed)
   }
 
   ngOnInit() {
 
     const leftArrow$ = fromEvent(document, 'keydown')
       .filter((event: KeyboardEvent) => event.key === 'ArrowLeft')
-      .map(event => this.calculateNewPosition(this.position, {x: this.speed, y: 0}));
+      .map(event => this.moveBackgroundIfOutOfBounds(this.position, {x: this.speed, y: 0}, this.charScreenPositionX, 21));
     const rightArrow$ = fromEvent(document, 'keydown')
       .filter((event: KeyboardEvent) => event.key === 'ArrowRight')
-      .map(event => this.calculateNewPosition(this.position, {x: -this.speed, y: 0}));
+      .map(event => this.moveBackgroundIfOutOfBounds(this.position, {x: -this.speed, y: 0}, this.charScreenPositionX, 79));
 
     const move$ = merge(leftArrow$, rightArrow$);
     const animationFrame$ = Observable.interval(0, Scheduler.animationFrame);
