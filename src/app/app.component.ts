816df16fe30792aaffc3d767806a74e7f09cb1cd
Chris Conolly <chris.conolly@wonga.com>
2018-04-20 08:40:47 +0100

    App now gets a config file and sets role from it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  role = 'Senior Software Engineer';
+  role;
+  background;
+  distance;
+
+  constructor(private http: Http) {
+    var obj;
+    this.getJSON().subscribe(data => obj = data, error => console.log(error));
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get("/assets/levels/senior-software-engineer.json")
+      .map((res: any) => {
+        const response = res.json();
+        this.role = response.role;
+        this.distance = response.distance;
+      })
+      .catch((error: any) => console.log(error));
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
@@ -54,7 +56,9 @@ import { LevelEndComponent } from './components/level-end/level-end.component'
     LevelEndComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [
     LerpService,
